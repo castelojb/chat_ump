2b7a56299902e338713b355c048d76ce52dfced5
João Castelo Branco <joaocb14@gmail.com>
2024-02-23 21:11:29 -0300

    fix: change secret access

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,8 +1,11 @@
 import os
+import streamlit as st
 
 
 def get_env(env_name: str) -> str:
     if env_var := os.environ.get(env_name):
+        return env_var
+    if env_var := st.secrets[env_var]:
         return env_var
 
     raise Exception(f"Missing database setting {env_name}")
